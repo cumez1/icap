3fd42d438b1918a44a5564c631c29098ac3d7971
Nicolas Cumez <cumez.1@gmail.com>
2022-01-15 09:28:42 -0600

    Add: custom css and img

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -75,7 +75,8 @@
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                                 <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> 
+                                    <span class="pt-2 ml-2">{{Auth::user()->name}}</span>
                                 </button>
                             @else
                                 <span class="inline-flex rounded-md">
--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,10 +1,10 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
     <div>
-        <x-jet-application-logo class="block h-12 w-auto" />
+        <x-jet-application-logo class="block h-20 w-auto" />
     </div>
 
     <div class="mt-8 text-2xl">
-        Welcome to your Jetstream application!
+        Bienvenido al sistema - <strong>ICAP Global Health</strong>
     </div>
 
     <div class="mt-6 text-gray-500">
